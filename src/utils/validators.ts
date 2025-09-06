f40e4e2ce91e6778a3a74f317948bd7eb473e0c5
NERFTHISPLS <39591372+NERFTHISPLS@users.noreply.github.com>
2025-09-06 12:30:31 +0300

    fix space inputs when creating a task

--- src/utils/validators.ts
+++ src/utils/validators.ts
@@ -1,7 +1,7 @@
 import { MAX_TASK_TEXT_LENGTH, MIN_TASK_TEXT_LENGTH } from './constants';
 
 export function validateTaskText(taskText: string): boolean {
-  const taskTextLength = taskText.length;
+  const taskTextLength = taskText.trim().length;
 
   return (
     taskTextLength >= MIN_TASK_TEXT_LENGTH &&
