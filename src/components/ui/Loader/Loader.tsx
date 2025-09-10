85035fb04a1d4e069e8f95f9484a9927b4c1c313
NERFTHISPLS <mustaevs@gmail.com>
2025-09-10 17:24:39 +0300

    remove unnecessary files

--- src/components/ui/Loader/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import styles from './Loader.module.scss';
-import type { JSX } from 'react';
-
-function Loader(): JSX.Element {
-  return <div className={styles.loader}></div>;
-}
-
-export default Loader;
