d525239c13a7c6e3229b9cac909f35ccc29aff19
NERFTHISPLS <mustaevs@gmail.com>
2025-09-22 20:39:43 +0300

    search, filter by blocked status and sort users

--- src/pages/UsersPage/UsersPage.tsx
+++ src/pages/UsersPage/UsersPage.tsx
@@ -1,17 +1,25 @@
-import type { GetUsersResponse, Role, User } from '@/types/user';
+import type { GetUsersResponse, Role, User, UserFilters } from '@/types/user';
 import {
   Alert,
   Button,
+  Dropdown,
   Flex,
+  Input,
   Space,
   Table,
   Tag,
   Tooltip,
+  type InputProps,
+  type MenuProps,
   type TableProps,
 } from 'antd';
-import { ArrowRightOutlined } from '@ant-design/icons';
+import {
+  ArrowRightOutlined,
+  FilterOutlined,
+  SearchOutlined,
+} from '@ant-design/icons';
 import { useNavigate } from 'react-router';
-import { useEffect, type JSX } from 'react';
+import { useEffect, useRef, useState, type JSX } from 'react';
 import { useAppDispatch, useAppSelector } from '@/store/hooks';
 import { selectUsers } from '@/store/selectors';
 import type { AsyncRequestData } from '@/store/utils';
@@ -25,7 +33,14 @@ const roleLabelsMap: Record<Role, string> = {
   MODERATOR: 'Moderator',
 };
 
+const filterMenuItems: MenuProps['items'] = [
+  { key: 'all', label: 'All' },
+  { key: 'blocked', label: 'Blocked' },
+  { key: 'not-blocked', label: 'Not blocked' },
+];
+
 const USERS_PER_PAGE = 20;
+const DEBOUNCE_MS = 300;
 
 function UsersPage(): JSX.Element {
   const {
@@ -35,14 +50,27 @@ function UsersPage(): JSX.Element {
   }: AsyncRequestData<GetUsersResponse> = useAppSelector(selectUsers);
   const dispatch = useAppDispatch();
   const navigate = useNavigate();
+  const searchTimeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+  const [filters, setFilters] = useState<UserFilters>({
+    page: 0,
+    limit: USERS_PER_PAGE,
+  });
 
   useEffect(() => {
     async function getUsers(): Promise<void> {
-      await dispatch(fetchUsers({ limit: USERS_PER_PAGE, page: 0 }));
+      await dispatch(fetchUsers(filters));
     }
 
     getUsers();
-  }, [dispatch]);
+
+    const timeoutId = searchTimeoutRef.current;
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [dispatch, filters]);
 
   if (error) {
     return (
@@ -60,15 +88,98 @@ function UsersPage(): JSX.Element {
 
   const showPagination = totalUsers > USERS_PER_PAGE;
 
-  const handleChangePage: TableProps<User>['onChange'] = async (pagination) => {
-    await dispatch(
-      fetchUsers({ limit: USERS_PER_PAGE, page: (pagination.current ?? 1) - 1 })
-    );
+  const handleTablePage: TableProps<User>['onChange'] = (
+    pagination,
+    _,
+    sorter
+  ) => {
+    const newPage = pagination.current ?? 1;
+    const currentPage = (filters.page ?? 0) + 1;
+
+    if (newPage !== currentPage) {
+      setFilters((filters) => ({
+        ...filters,
+        page: newPage - 1,
+      }));
+    }
+
+    if (Array.isArray(sorter)) {
+      return;
+    }
+
+    if (
+      String(sorter.field) !== filters.sortBy ||
+      sorter.order !== filters.sortOrder
+    ) {
+      const sortOrderServer: UserFilters['sortOrder'] =
+        sorter.order === 'ascend'
+          ? 'asc'
+          : sorter.order === 'descend'
+          ? 'desc'
+          : undefined;
+
+      setFilters((filters) => ({
+        ...filters,
+        page: 0,
+        sortBy: String(sorter.field),
+        sortOrder: sortOrderServer,
+      }));
+    }
+  };
+
+  const handleSearch: InputProps['onChange'] = (e) => {
+    const value = e.target.value.trim();
+
+    if (searchTimeoutRef.current) {
+      clearTimeout(searchTimeoutRef.current);
+    }
+
+    searchTimeoutRef.current = setTimeout(() => {
+      setFilters((filters) => ({ ...filters, search: value, page: 0 }));
+    }, DEBOUNCE_MS);
+  };
+
+  const handleFilterChange: MenuProps['onClick'] = ({ key }) => {
+    const filterIsBlocked = key === 'all' ? undefined : key === 'blocked';
+
+    if (filterIsBlocked === filters.isBlocked) {
+      return;
+    }
+
+    setFilters((filters) => ({
+      ...filters,
+      isBlocked: filterIsBlocked,
+      page: 0,
+    }));
   };
 
   return (
     <Flex vertical gap="middle" style={{ padding: '1rem' }}>
       <h2>Users Info</h2>
+
+      <Flex gap="small">
+        <Input
+          prefix={<SearchOutlined />}
+          placeholder="Search by username or email..."
+          onChange={handleSearch}
+        />
+
+        <Dropdown
+          menu={{
+            items: filterMenuItems,
+            selectable: true,
+            defaultSelectedKeys: ['all'],
+            onClick: handleFilterChange,
+          }}
+        >
+          <Button>
+            <Space>
+              <FilterOutlined />
+              Filter
+            </Space>
+          </Button>
+        </Dropdown>
+      </Flex>
 
       <Table<User>
         dataSource={data?.data ?? []}
@@ -79,18 +190,20 @@ function UsersPage(): JSX.Element {
           position: [showPagination ? 'bottomCenter' : 'none'],
           pageSize: USERS_PER_PAGE,
           showSizeChanger: false,
+          current: (filters.page ?? 0) + 1,
         }}
         scroll={{ x: 'max-content', scrollToFirstRowOnChange: true }}
-        onChange={handleChangePage}
+        onChange={handleTablePage}
       >
         <Column
           title="Username"
           dataIndex="username"
           key="username"
           fixed="left"
+          sorter={true}
         />
 
-        <Column title="Email" dataIndex="email" key="email" />
+        <Column title="Email" dataIndex="email" key="email" sorter={true} />
 
         <Column
           title="Registration date"
